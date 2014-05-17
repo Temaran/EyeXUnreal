bf6e96eb1122d067ba0e039680ceca189badb8e4
Temaran <temaran@gmail.com>
2014-05-17 08:09:44 +0200

    Updated file headers

--- Source/EyeXEyetracking/EyeXEyetracking.Build.cs
+++ Source/EyeXEyetracking/EyeXEyetracking.Build.cs
@@ -1,3 +1,14 @@
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Created by Fredrik Lindh (Temaran)
+// Contact me at: temaran (at) gmail (dot) com
+// Last changed: 2014-05-17
+// You're free to do whatever you want with the code except claiming it's you who wrote it.
+// I would also appreciate it if you kept this file header as a thank you for the code :)
+
 using System.IO;
 
 namespace UnrealBuildTool.Rules
--- Source/EyeXEyetracking/Private/EyeXHost.h
+++ Source/EyeXEyetracking/Private/EyeXHost.h
@@ -1,10 +1,13 @@
-/*
- * FEyeXHost class: Responsible for the gaze interaction within a window.
- * Holds the current set of activatable regions and acts as a simple interactor repository.
- * Sends notifications as Windows messages so that they are received on the main thread and can be handled there.
- *
- * Copyright 2013 Tobii Technology AB. All rights reserved.
- */
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Created by Fredrik Lindh (Temaran)
+// Contact me at: temaran (at) gmail (dot) com
+// Last changed: 2014-05-17
+// You're free to do whatever you want with the code except claiming it's you who wrote it.
+// I would also appreciate it if you kept this file header as a thank you for the code :)
 
 #pragma once
 
